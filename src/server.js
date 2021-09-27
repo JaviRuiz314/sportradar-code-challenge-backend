72dc2d57055909e51f74390e94e765fe13181ece
Javier Ruiz de Valdivia <javiruiz314@gmail.com>
2021-09-27 08:41:46 +0200

    add cors library

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@
 const 
 	express = require('express'),
 	app = express(),
+	cors = require('cors'),
 	marked = require('marked'),
 	routes = require('./routes'),
 	fs = require('fs');
+
+	app.use(cors());
 
 	app.get('/', function (req, res) {
 		const
